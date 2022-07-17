3e5fd0ea3da91ba29802987405d7a4807a696275
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-07-17 18:56:47 -0400

    inject .env vars at app module root

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { Authmodule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { SearchModule } from './search/search.module';
 import { CollectionModule } from './collection/collection.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [Authmodule, UserModule, SearchModule, CollectionModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    SearchModule,
+    CollectionModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
